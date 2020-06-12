576ca773b41570aef2a24e5e83f8f6f761481089
Josué Cesar <mendonca.josue@gmail.com>
2020-06-11 22:24:21 -0300

    User controller e criação de usuário prontos

---
+++ src/controllers/LocationsController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+class UsersController {
+  async create(req: Request, res: Response) {}
+}
+
+export default UsersController;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 
+import UsersController from '../controllers/UsersController';
+
+const usersController = new UsersController();
+
 const routes = Router();
 
 routes.get('/', (req, res) => res.json({ message: 'ONRINE MERMAO' }));
+routes.post('/user', usersController.create);
 
 export default routes;
