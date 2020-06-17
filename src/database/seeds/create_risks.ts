b28e1eac4a671dc7738cb0d2337daf20ed27cc4e
ThiagoCRDM <thiagohs619@gmail.com>
2020-06-17 11:54:01 -0300

    coluna local

--- src/database/seeds/create_risks.ts
+++ src/database/seeds/create_risks.ts
@@ -6,5 +6,6 @@ export async function seed(knex: Knex) {
     { risk: 'Chuva', latitude: -19.9719992, longitude: -44.0982057 },
     { risk: 'Desastre', latitude: -19.9769323, longitude: -44.2170795 },
     { risk: 'Desastre', latitude: -19.9721013, longitude: -44.1899566 },
+    { risk: 'Risco', latitude: -19.918967, longitude: -44.278804 },
   ]);
 }
